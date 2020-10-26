aa71d76a8c3a0f166c8bdf143a35a2ba137ed88b
Jian Qiu <jqiu@redhat.com>
2020-10-26 09:53:22 +0800

    Initial commit to the website

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "Deploying updates to open-cluster-management.github.io"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
